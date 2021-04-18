a8358ece6458113a30064d7fcfb6b2e894950cbf
Amila Weerasinghe <40320449+AmilaWeerasinghe@users.noreply.github.com>
2021-04-19 00:30:15 +0530

    Add files via upload

---
+++ code/Dist/client.py
@@ -0,0 +1,77 @@
+import pickle
+import json
+import socket
+import numpy as np
+import struct
+import time
+from threading import *
+import yolo as y
+import weights as w
+
+class client(Thread):
+	payload_size = struct.calcsize("L")  ### CHANGED
+	data = b''
+	valdict=0
+	x=0
+	def __init__(self,values):
+		super(client, self).__init__()
+		self.valdict=values
+
+	def send(self,c,data):
+		data_string=pickle.dumps(data)
+		message_size = struct.pack("L", len(data_string))
+		c.sendall(message_size+data_string)
+	def receive_array(self,data,payload_size,conn):
+		while len(data) < payload_size:
+			data += conn.recv(4096)
+
+		packed_msg_size = data[:payload_size]
+		data = data[payload_size:]
+		msg_size = struct.unpack("L", packed_msg_size)[0]
+
+        # Retrieve all data based on message size
+		while len(data) < msg_size:
+			data += conn.recv(4096)
+
+		frame_data = data[:msg_size]
+		data = data[msg_size:]
+
+        # Extract frame
+		frame = pickle.loads(frame_data)
+		return frame
+
+	def run(self):
+		c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		c.connect(('localhost',9999)) #ip address and port
+		self.send(c,self.valdict)
+		self.x=self.receive_array(self.data,self.payload_size,c)
+	def value(self):
+		return self.x
+
+
+#c.connect(('localhost',9999)) #ip address and port
+#send data to server
+#send(c,conv_dict)
+#receive data from server
+#out=receive_array(data,payload_size,c)
+#print(out["data"].shape)
+np.random.seed(1)
+image=np.random.randn(1,256,256, 3) #h256X256 image
+a=round(w.W1.shape[3]/2)
+conv_dict = { "data":image,"hpara":w.hparameters1,"pos":a}
+#tic = time.process_time()
+c=client(conv_dict)
+c.start()
+#client process conv portion
+tic = time.process_time()
+out=y.conv_forward(image, w.W1[:,:,:,:a], w.b1[:,:,:,:a], w.hparameters1)
+toc = time.process_time()
+print ("Computation time conv part1 = " + str(1000*(toc - tic)) + "ms")
+tic = time.process_time()
+c.join()
+print(c.value()["data"].shape)
+toc = time.process_time()
+out1=np.concatenate((out,c.value()["data"]), axis=3)
+#toc = time.process_time()
+print ("Computation time for join = " + str(1000*(toc - tic)) + "ms")
+print("Out1 shape",out1.shape)
---
+++ code/Dist/yolo.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pymp
+
+
+def softmax(Z):
+    """
+    Softmax activation function, vectorized version (array Z).
+    Args:
+        Z (ndarray): numpy array of any shape, output of the linear layer
+    """
+    Z_exp = np.exp(Z)
+    A = Z_exp / np.sum(Z_exp, axis=1,keepdims=True)
+
+    assert (A.shape == Z.shape)
+
+    return A
+
+def relu(Z):
+    """
+    ReLU activation function, vectorized version (array Z).
+    Args:
+        Z (ndarray): numpy array of any shape, output of the linear layer
+    Returns:
+        A (ndarray): post-activation output of relu(Z), same shape as Z
+    """
+    A = np.maximum(0, Z)
+
+    assert (A.shape == Z.shape)
+
+    return A
+
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image,
+    as illustrated in Figure 1.
+
+    Argument:
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+
+    Returns:
+    X_pad -- padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    """
+    X_pad =np.pad(X, ((0,0),(pad,pad),(pad,pad),(0,0)), mode='constant', constant_values = (0,0))
+
+
+    return X_pad
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation
+    of the previous layer.
+
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+
+    Returns:
+    Z -- a scalar value, the result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+
+    # Element-wise product between a_slice_prev and W. Do not add the bias yet.
+    s = a_slice_prev*W
+    # Sum over all entries of the volume s.
+    Z = s.sum()
+    # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
+    Z = Z+float(b)
+
+
+    return Z
+ 
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+
+    Arguments:
+    A_prev -- output activations of the previous layer,
+        numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function
+    """
+
+    # Retrieve dimensions from A_prev's shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve dimensions from W's shape
+    (f, f, n_C_prev, n_C) = W.shape
+
+    # Retrieve information from "hparameters"
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+
+    # Compute the dimensions of the CONV output volume using the formula given above.
+    # Hint: use int() to apply the 'floor' operation.
+    n_H = int((n_H_prev-f+2*pad)/stride+1)
+    n_W = int((n_W_prev-f+2*pad)/stride+1)
+
+    # Initialize the output volume Z with zeros.
+    Z = np.zeros((m,n_H,n_W,n_C))
+
+    # Create A_prev_pad by padding A_prev
+    A_prev_pad =zero_pad(A_prev, pad)
+
+
+    with pymp.Parallel(2) as p: 
+      for i in p.range(m):               # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]               # Select ith training example's padded activation
+        for h in p.range(n_H):           # loop over vertical axis of the output volume
+            vert_start = h*stride
+            vert_end = vert_start+f
+
+            for w in p.range(n_W):       # loop over horizontal axis of the output volume
+                #Find the horizontal start and end of the current "slice" (≈2 lines)
+                horiz_start = w*stride
+                horiz_end = horiz_start+f
+
+        #iterate over the filters
+            #with pymp.Parallel(2) as p: 
+                for c in p.range(n_C):   # loop over channels (= #filters) of the output volume
+
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+
+                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈3 line)
+                    weights = W[:,:,:,c]
+                    biases = b[:,:,:,c]
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev,weights,biases)
+
+
+
+    # Making sure your output shape is correct
+    assert(Z.shape == (m, n_H, n_W, n_C))
+
+    # Save information in "cache" for the backprop
+    #cache = (A_prev, W, b, hparameters)
+
+    return Z#, cache
+
+# GRADED FUNCTION: pool_forward
+
+def pool_forward(A_prev, hparameters, mode = "max"):
+    """
+    Implements the forward pass of the pooling layer
+
+    Arguments:
+    A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    hparameters -- python dictionary containing "f" and "stride"
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+
+    Returns:
+    A -- output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters
+    """
+
+    # Retrieve dimensions from the input shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve hyperparameters from "hparameters"
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+
+    # Define the dimensions of the output
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+
+    # Initialize output matrix A
+    A = np.zeros((m, n_H, n_W, n_C))
+
+    for i in range(m):                         # loop over the training examples
+        for h in range(n_H):                     # loop on the vertical axis of the output volume
+            # Find the vertical start and end of the current "slice" (≈2 lines)
+            vert_start = h*stride
+            vert_end = vert_start+f
+
+            for w in range(n_W):                 # loop on the horizontal axis of the output volume
+                # Find the vertical start and end of the current "slice" (≈2 lines)
+                horiz_start = w*stride
+                horiz_end = horiz_start+f
+
+                for c in range (n_C):            # loop over the channels of the output volume
+
+                    # Use the corners to define the current slice on the ith training example of A_prev, channel c. (≈1 line)
+                    a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+
+                    # Compute the pooling operation on the slice.
+                    # Use an if statement to differentiate the modes.
+                    # Use np.max and np.mean.
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(a_prev_slice)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(a_prev_slice)
+
+
+
+    # Store the input and hparameters in "cache" for pool_backward()
+    #cache = (A_prev, hparameters)
+
+    # Making sure your output shape is correct
+    #assert(A.shape == (m, n_H, n_W, n_C))
+
+    return A#, cache
+
+
+"""
+#test
+np.random.seed(1)
+A_prev = np.random.randn(2, 5, 5, 3)
+hparameters = {"stride" : 1, "f": 3}
+
+A= pool_forward(A_prev, hparameters)
+print("mode = max")
+print("A.shape = " + str(A.shape))
+
+
+#softmax test
+
+x = np.array([
+    [9, 2, 5, 0, 0],
+    [7, 5, 0, 0 ,0]])
+print("softmax(x) = " + str(softmax(x)))
+"""
+"""
+expected output
+[
+ [  9.80897665e-01   8.94462891e-04   1.79657674e-02   1.21052389e-04 1.21052389e-04]
+ [  8.78679856e-01   1.18916387e-01   8.01252314e-04   8.01252314e-04 8.01252314e-04]
+]
+
+"""
